5999b54cd9c6e7b46782943069400e611ee8725d
Chris Conover <clcono@gmail.com>
2012-07-18 14:27:58 -0400

    Do below the fold height resize after everything loads

--- static/js/script.js
+++ static/js/script.js
@@ -88,7 +88,8 @@ if (typeof jQuery != 'undefined'){
 		/* Theme Specific Code Here */
 		//Generic.homeDimensions($);
 		//Generic.resizeSearch($);
-
-		$('#below-the-fold .center, #below-the-fold .right').height($('#below-the-fold').height() - 15);
+		$(window).load(function() {
+			$('#below-the-fold .center, #below-the-fold .right').height($('#below-the-fold').height() - 15);
+		});
 	});
 }else{console.log('jQuery dependancy failed to load');}
