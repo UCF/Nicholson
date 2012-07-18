0d6cf380b0c4e4c63335066d9bef9765456e1d05
Chris Conover <clcono@gmail.com>
2012-07-18 09:37:15 -0400

    Adjusted subpage markup

--- includes/subpage.php
+++ includes/subpage.php
@@ -1,7 +1,15 @@
 <?php get_header(); the_post();?>
 	<div class="page-content">
-		<h2><?=$post->post_title?></h2>
-		<?=$content = str_replace(']]>', ']]>', apply_filters('the_content', $post->post_content))?>
-		<?php get_template_part('includes/below-the-fold'); ?>
+		<div class="row">
+			<div class="span12">
+				<h2><?=$post->post_title?></h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span12">
+				<?=$content = str_replace(']]>', ']]>', apply_filters('the_content', $post->post_content))?>
+				<?php get_template_part('includes/below-the-fold'); ?>
+			</div>
+		</div>
 	</div>
 <?php get_footer();?>
