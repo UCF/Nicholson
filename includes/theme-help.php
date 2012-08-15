78bcc3d630bf56e6cd77e35e25f849a22eacfa56
Chris Conover <clcono@gmail.com>
2012-08-15 15:45:26 -0400

    Updated documentation

--- includes/theme-help.php
+++ includes/theme-help.php
@@ -8,80 +8,78 @@
 		
 		<div class="sections">
 			<ul>
-				<li class="section"><a href="#posting">Posting</a></li>
+				<li class="section"><a href="#general">General</a></li>
 				<li class="section"><a href="#shortcodes">Shortcodes</a></li>
 			</ul>
 		</div>
 		<div class="fields">
 			<ul>
-				
-				<li class="section" id="posting">
-					<h3>Posting</h3>
-					<p>Posting is fun, do it.</p>
+				<li class="section" id="general">
+					<h3>General Information</h3>
+					<h4>Github</h4>
+					<p>The theme is currently hosted on Github at <a href="https://github.com/UCF/Nicholson">https://github.com/UCF/Nicholson</a>. We can either grant Nicholson personnel access to the /UCF repository or it can be forked into the Nichoslson account.</p>
+
+					<h4>Bootstrap</h4>
+					<p>This theme is based on Twitter's Bootstrap HTML, CSS, and JavaScript framework located here: <a href="http://twitter.github.com/bootstrap/">http://twitter.github.com/bootstrap/</a>. Anything currently possible in the Bootstrap examples is possible in this theme.</p>
+
+					<h4>Support</h4>
+					<p>If you have any questions or problems, you can email the general Web Communications Team email <a href="mailto:webcom@ucf.edu">webcom@ucf.edu</a> or contact any individual directly.</p>
 				</li>
-				
 				<li class="section" id="shortcodes">
 					<h3>Shortcodes</h3>
 					
-					<h4>slideshow</h4>
-					<p>Create a javascript slideshow of each top level element in the shortcode.  All attributes are optional, but may default to less than ideal values.  Available attributes:</p>
-					<table>
-						<tr>
-							<th scope="col">Name</th>
-							<th scope="col">Description</th>
-							<th scope="col">Default Value</th>
-						</tr>
-						<tr>
-							<td>height</td>
-							<td>CSS height of the outputted slideshow</td>
-							<td>100px</td>
-						</tr>
-						<tr>
-							<td>width</td>
-							<td>CSS width of the outputted slideshow</th>
-							<td>100%</td>
-						</tr>
-						<tr>
-							<td>transition</td>
-							<td>Length of transition in milliseconds</td>
-							<td>1000</td>
-						</tr>
-						<tr>
-							<td>cycle</td>
-							<td>Length of each cycle in milliseconds</td>
-							<td>5000</td>
-						</tr>
-						<tr>
-							<td>animation</td>
-							<td>The animation type, one of: 'slide' or 'fade'</td>
-							<td>slide</td>
-						</tr>
-					</table>
-					<p>Example:
-<pre><code>[slideshow height="500px" transition="500" cycle="2000"]
-&lt;img src="http://some.image.com" .../&gt;
-&lt;div class="robots"&gt;Robots are coming!&lt;/div&gt;
-&lt;p&gt;I'm a slide!&lt;/p&gt;
-[/slideshow]</code></pre>
-					
-					<h4>document-list</h4>
-					<p>Outputs a list of {$plural} filtered by arbitrary taxonomies, for example a tag
-					or category.  A default output for when no objects matching the criteria are
-					found.</p>
-					<p>Example:</p>
-<pre><code># Output a maximum of 5 items tagged foo or bar, with a default output.
-[{$scode} tags="foo bar" limit="5"]No {$plural} were found.[/{$scode}]
+					<h4>Generic Shortcode</h4>
+					<p>Any post type (Posts, Pages, Front Page Centerpieces, People, and Documents) can be outputted using a shortcode of the form <post type>-list. The <post type> placeholder is the name of the post type as specified in custom_post_types.php. For example, to output a list of People posts the shortcode would look like the following: [person-list]. </p>
 
-# Output all objects categorized as foo
-[{$scode} categories="foo"]
+					<p>This generic shortcode can take the following arguments although none are required:</p>
+					<ul style="margin-left:20px;">
+						<li><strong>limit</strong> – How many posts are displayed. All are displayed by default.</li>
+						<li><strong>class</strong> – Additional CSS classes to add to the surrounding element. Empty by default.</li>
+						<li><strong>orderby</strong> – Which field or fields the posts will be ordered by. By default: menu_order and title. For more details on the orderby parameter, see the following WordPress documentation: http://codex.wordpress.org/Class_Reference/WP_Query#Order_.26_Orderby_Parameters</li>
+						<li><strong>order</strong> – Which direction the posts are ordered in. Default is ascending. The possible options are ASC and DESC.</li>
+						<li><strong>Taxonomies</strong> - There are four taxonomy arguments in this theme. Passing one or more of these arguments will cause only posts that exist in the specified taxonomies to appear in the output. All taxonomies must be specified in slug format. The available taxonomies are:
+							<ul style="margin-left:40px;">
+								<li><strong>tags</strong></li>
+								<li><strong>categories</strong></li>
+								<li><strong>org_groups</strong></li>
+								<li><strong>document_groups</strong></li>
+							</ul>
+						</li>
+					</ul>
+					<p>As an example, the shortcode to display six random people from the Executive organizational group would be: [person-list limit=”6” orderby=”rand” org_groups=”executive”]</p>
 
-# Output all objects matching the terms in the custom taxonomy named foo
-[{$scode} foo="term list example"]
+					<p>While the default styling for the generic shortcode is an unstyled list, some post types have different styling. These include:</p>
+					<ul style="margin-left:20px;">
+						<li><strong>document</strong> – An unstyled list with a small icon indicating the document type next to each list item.</li>
+						<li><strong>person</strong> – A table with the following columns: name, job title, phone number and email.</li>
+						<li><strong>post</strong> – Each post is a stylized box that is 4 columns wide and contains the post’s featured image (if one exists), the post title, the post excerpt, the post creation date and time, and the categories that the post belongs to.</li>
+					</ul>
 
-# Outputs all objects found categorized as staff and tagged as small.
-[{$scode} limit="5" join="and" categories="staff" tags="small"]</code></pre>
+					<h4>Person Picture List (person-picture-list)</h4>
+					<p>This shortcode is very similar to the generic shortcode [person-list] except that it outputs the profile image of the person, name, and job title in a row format instead of a table. In addition to all the arguments that the [person-list] shortcode takes, this shortcode also takes an optional row-size argument that controls how many people will be displayed in each row. The default is five.</p>
+
+					<h4>Shadow Well (shadow-well)</h4>
+					<p>This shortcodes wraps the specified content in a stylized, shadow box. It takes three optional arguments:</p>
+					<ul style="margin-left:20px;">
+						<li>width – Width of the well in columns. Default is 12</li>
+						<li>wrap – Whether or not to include the bootstrap row wrap markup. Default is true.</li>
+						<li>gold – Whether to use a gold background color instead of white. The default is false.</li>
+					</ul>
+					<p>As an example, the shortcode to style the content “This is a shadow well!” in a shadow well would be: [shadow-well]This is a shadow well![/shadow-well]</p>
+
+					<h4>Titled Section (titled-section)</h4>
+					<p>This shortcode wraps the specific content in markup that adds a gold header bar to the section. This shortcode takes one required argument: title.</p>
+
+					<h4>Category List (category-list)</h4>
+					<p>This shortcode outputs a list of post categories in a series of unstyled lists divided into columns. Each item is a link to the category’s listing page. This shortcode takes two optional arguments:</p>
+					<ul style="margin-left:20px">
+						<li><strong>columns</strong> – The number of columns the categories should be split into. The default is three.</li>
+						<li><strong>width</strong> – The width of each of the columns in spans. The default is two.</li>
+					</ul>
+
+					<h4>Archive Year list (archive-year-list)</h4>
+					<p>This shortcode outputs an unstyled list of archive years that link to the archive pages.</p>
 				</li>
-				
 			</ul>
 		</div>
 	</div>
